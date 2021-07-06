e59c5709a3139a8807e9ddb208bd05b8a7cf119a
Schiotter <67166450+Schiotter@users.noreply.github.com>
2021-07-06 16:20:50 +0200

    Create example.php

---
+++ example.php
@@ -0,0 +1,25 @@
+<?php
+
+//Can be run in a Browser or on the CLI!
+
+require 'class.mojangapi.php';
+
+if(php_sapi_name() === 'cli') {
+    if(empty($argv[1])) {
+        die("missing argv[1]!\n");
+    } else {
+        $_GET['name'] = $argv[1];
+    }
+} else {
+    if(empty($_GET['name'])) {
+        $form = '<form><input name="name" type="name"><input type="submit"></form>';
+        die($form);
+    }
+}
+
+$mapi = new mojangapi;
+$data = $mapi->getbyName($_GET['name']);
+
+header('Content-Type: application/json'); 
+print json_encode($data, JSON_PRETTY_PRINT);
+?>
